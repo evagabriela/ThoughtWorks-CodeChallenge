68f624d4ff68e7f0157973adb0cab2f79f65ffeb
Eva Zamudio <gabriela.osu@gmail.com>
2013-12-01 13:50:31 -0800

    add driver code

--- lib/navigation_controller.rb
+++ lib/navigation_controller.rb
@@ -1,3 +1,21 @@
-# Developer : Eva Gabriela Zamudio 
+#Developer : Eva Gabriela Zamudio 
 require_relative 'rover'
 require_relative 'plateau'
+
+#Initialize a new plateau in Mars for the upper-right coordinates of the plateau
+plateau = Plateau.new(5,5)
+#Initialize two rovers to place on the plateau
+rover1 = Rover.new(1,2,'N')  
+rover2 = Rover.new(3,3,'E')
+#Print out rover's initial position
+rover1.print_initial_position 
+rover2.print_initial_position
+#Add rovers to the plateau
+plateau.add_rover(rover1)
+plateau.add_rover(rover2)
+#Moving the rover based on input
+rover1.move("LMLMLMLMM") 
+rover2.move("MMRMMRMRRM")
+#Ending positions
+rover1.ending_position
+rover2.ending_position
