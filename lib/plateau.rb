75a07b67141cedb5169433e4c48c83784f8df59a
Eva Zamudio <gabriela.osu@gmail.com>
2013-12-01 15:06:28 -0800

    add main code

--- lib/plateau.rb
+++ lib/plateau.rb
@@ -1,8 +1,17 @@
 # Developer : Eva Gabriela Zamudio 
 
 class Plateau
-  def initialize()
-    
+  attr_reader :rovers, :x_limit, :y_limit
+  
+  def initialize(x,y)
+    @x_limit = x
+    @y_limit = y
+    @rovers = []
+  end
+
+  def add_rover(rover)
+    @rovers << rover
+    rover.plateau = self
   end
 
 end 
