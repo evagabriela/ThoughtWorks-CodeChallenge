b7074f9d738f97b37077e0862245a41b06aa32c1
Eva Zamudio <gabriela.osu@gmail.com>
2013-12-02 13:50:01 -0800

    add code for testing

--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
-require_relative '..lib/plateau'
+require 'plateau'
 
 
 describe Plateau do 
+  let(:plateau) {Plateau.new(5,5)}
+
+  describe "#add_rover" do
+    let(:rover) {double(:rover, pos_x: 1, pos_y: 2, direction: 'N').as_null_object}
+    it "should add a rover to a plateau" do
+      plateau.add_rover(rover)
+      plateau.rovers.should include(rover)
+    end
+  end
 end
